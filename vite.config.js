c492e9a5d042258b5d956322bcc5f383b2870ab0
Mizu <psdk55052004@gmail.com>
2023-11-28 17:07:54 +0800

    修改 設定檔的專案名稱

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-good-table/', //! 請輸入儲存庫名稱
+  base: '/vue-good-table-next/', //! 請輸入儲存庫名稱
   plugins: [vue()],
   resolve: {
     alias: [
